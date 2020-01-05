8145e5f24d9f3df1f56b1eadb72d1acaf98bdba3
Dina <dinabag@mail.ru>
2020-01-06 00:27:37 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Singers from "./Singers/Singers";
+import Discography from "./Discography/Discography";
+import Contact from "./Contact/Contact";
+import { Element  } from "react-scroll";
 
-function App() {
+const App=()=> {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Element name='main'>
+      <Main />
+      </Element>
+      <Element name='singers'>
+      <Singers />
+      </Element>
+        <Element name='discography'>
+      <Discography/>
+      </Element>
+        <Element name='contact'>
+      <Contact/>
+        </Element>
     </div>
   );
 }
